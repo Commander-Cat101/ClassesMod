22f9cd5282f533f10add2594d5435aba04c62c33
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2022-10-25 17:11:19 +1300

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -8,6 +8,6 @@ public static class ModHelperData
     public const string Description = "Adds a bunch of classes to BTD6";
 
     public const string RepoOwner = "Commander-Cat101"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "ClassesMod"; // TODO add your repo name here, also in the download url in README.md
     public const string Author = "Commander__Cat";
 }

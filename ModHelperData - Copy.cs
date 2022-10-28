61d35573a8e5bb5d54b4bce40b2cfe11d2ad1e06
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2022-10-28 18:50:53 +1300

    Add files via upload

---
+++ ModHelperData - Copy.cs
@@ -0,0 +1,13 @@
+namespace BloonsTDClasses;
+
+public static class ModHelperData
+{
+    public const string Version = "1.0.0";
+    public const string Name = "BloonsTDClasses";
+
+    public const string Description = "Adds a bunch of classes to BTD6";
+
+    public const string RepoOwner = "Commander-Cat101"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "ClassesMod"; // TODO add your repo name here, also in the download url in README.md
+    public const string Author = "Commander__Cat";
+}
